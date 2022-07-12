706aff8085cf8ae01967d23fc9f05ca6088f37bc
Michel Futila <michelfutila@tikdem.com>
2022-07-13 00:01:55 +0100

    register part 4

--- netflix/users/views.py
+++ netflix/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
 # Create your views here.
 
+@csrf_protect
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  
+        if form.is_valid(): 
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,'{} votre comte a ete cree avec succes'.format(username))
+            return redirect('login')
+    else :
+        form = UserCreationForm()
     return render(request,"users/register.html",{"form_register" : form})
