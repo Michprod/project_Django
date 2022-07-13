10cf7ef9aecdb9ad0592236e9837969b5cee665e
Michel Futila <michelfutila@tikdem.com>
2022-07-13 13:22:24 +0100

    modif sur setting

--- netflix/netflix/settings.py
+++ netflix/netflix/settings.py
@@ -120,13 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/film_pic/'
 
-STATICFILES_DIRS = [
-       BASE_DIR / 'static'
-]
 
-MEDIA_ROOT = BASE_DIR / 'static/film_pic'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 
 #pour dire a crispy d'utileser bootstra^4
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
